21389bd6463801b5066e64f952b332455003c301
akashrathore94 <akashrathour2011@gmail.com>
2020-09-13 05:57:46 +0530

    script added

---
+++ script.js
@@ -0,0 +1,8 @@
+function navToggle() {
+  const navHead = document.querySelector(".nav-head");
+  navHead.classList.toggle("toggle");
+  var btn = document.querySelector(".toggle-btn");
+  btn.classList.toggle("toggle");
+  var navToggle = document.querySelector(".nav-toggle");
+  navToggle.classList.toggle("toggle");
+}
