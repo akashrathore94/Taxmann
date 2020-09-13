fecfc57790a4bb0778e2e4150ce606c14a1de531
akashrathore94 <akashrathour2011@gmail.com>
2020-09-13 22:33:31 +0530

    js completed

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ function navToggle() {
   var navToggle = document.querySelector(".nav-toggle");
   navToggle.classList.toggle("toggle");
 }
+
+window.addEventListener("resize", myFunction);
+
+function hide() {
+  const navHead = document.querySelector(".nav-head");
+  const btn = document.querySelector(".toggle-btn");
+  const navToggle = document.querySelector(".nav-toggle");
+  navToggle.classList.add("toggle");
+
+  navHead.classList.add("toggle");
+  btn.classList.add("toggle");
+}
+
+function myFunction() {
+  let w = document.body.offsetWidth;
+  if (w <= 885) hide();
+}
