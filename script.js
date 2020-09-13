fb7c2176b3ad75c2896b86dd36e1a37385a9ca3e
akashrathore94 <akashrathour2011@gmail.com>
2020-09-13 23:23:31 +0530

    toggle menu completed

--- script.js
+++ script.js
@@ -7,16 +7,30 @@ function navToggle() {
   navToggle.classList.toggle("toggle");
 }
 
+function asideToggle() {
+  const asideHead = document.querySelector(".aside-head");
+  asideHead.classList.toggle("toggle");
+  var btn = document.querySelector(".aside-toggle-btn");
+  btn.classList.toggle("toggle");
+  var asideToggle = document.querySelector(".aside-nav-toggle");
+  asideToggle.classList.toggle("toggle");
+}
+
 window.addEventListener("resize", myFunction);
 
 function hide() {
   const navHead = document.querySelector(".nav-head");
+  const asideHead = document.querySelector(".aside-head");
   const btn = document.querySelector(".toggle-btn");
+  const asideBtn = document.querySelector(".aside-toggle-btn");
   const navToggle = document.querySelector(".nav-toggle");
+  const asideToggle = document.querySelector(".aside-nav-toggle");
   navToggle.classList.add("toggle");
-
+  asideToggle.classList.add("toggle");
   navHead.classList.add("toggle");
+  asideHead.classList.add("toggle");
   btn.classList.add("toggle");
+  asideBtn.classList.add("toggle");
 }
 
 function myFunction() {
